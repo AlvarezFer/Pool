216fecb5f1b79445842e71c0aca41c69a58740c4
AlvarezFer <fernicomeres@gmail.com>
2023-05-29 20:33:46 -0300

    Se reenderizan componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ColorSchemesExample from "./Components/Navbar";
+import Description from "./Components/Main";
+import Header from "./Components/Header";
+import Footer from "./Commons/Footer";
+import Contact from "./Components/Contact";
+import "./Styles/app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ColorSchemesExample />
+      <Header />
+      <Description />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
